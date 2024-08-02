a6b00c19c471267c84f3a4f3679fd20b26cc75dc
PyroX2 <kubawilk63@gmail.com>
2024-08-02 13:50:18 +0200

    Clean and add comments

--- run_inference.py
+++ run_inference.py
@@ -1,4 +1,4 @@
-from groundingdino.util.inference import load_model, load_image, predict, annotate
+from groundingdino.util.inference import load_model, predict, annotate
 import groundingdino.datasets.transforms as T
 import cv2
 from PIL import Image
@@ -6,43 +6,46 @@ from time import time
 import os
 
 
-
-model = load_model("GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "weights/groundingdino_swint_ogc.pth", device="cpu")
+# Set constants
 TEXT_PROMPT = "drone"
 BOX_TRESHOLD = 0.35
 TEXT_TRESHOLD = 0.25
 START_SEC = 4
 END_SEC = 5
 TAG = "cloudy"
-video_path = "/mnt/nas-data/HardKill/hardkill_videos/data_validation_videos/rgb_1080p"
-file_name = 'dsv_rgb_1080p_nalot_air_9s_cloudy'
+VIDEO_PATH = "/mnt/nas-data/HardKill/hardkill_videos/data_validation_videos/rgb_1080p"
+FILENAME = 'dsv_rgb_1080p_nalot_air_9s_cloudy'
 
-images_output_dir = os.path.join("outputs", TAG, file_name, "images")
-labels_output_dir = os.path.join("outputs", TAG, file_name, "labels")
-annotated_output_dir = os.path.join("outputs", TAG, file_name, "annotated")
+# Load model weights
+model = load_model("GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "weights/groundingdino_swint_ogc.pth", device="cpu")
+
+# Set output dirs and create them if they don't exist
+images_output_dir = os.path.join("outputs", TAG, FILENAME, "images")
+labels_output_dir = os.path.join("outputs", TAG, FILENAME, "labels")
+annotated_output_dir = os.path.join("outputs", TAG, FILENAME, "annotated")
 
 if not os.path.exists(images_output_dir):
-    os.makedirs(os.path.join("outputs", TAG, file_name, "images"))
+    os.makedirs(os.path.join("outputs", TAG, FILENAME, "images"))
 if not os.path.exists(labels_output_dir):
-    os.makedirs(os.path.join("outputs", TAG, file_name, "labels"))
+    os.makedirs(os.path.join("outputs", TAG, FILENAME, "labels"))
 if not os.path.exists(annotated_output_dir):
-    os.makedirs(os.path.join("outputs", TAG, file_name, "annotated"))
+    os.makedirs(os.path.join("outputs", TAG, FILENAME, "annotated"))
 
 
-full_name = file_name + ".mp4"
-cap = cv2.VideoCapture(os.path.join(video_path, full_name))
+# Capture video and get fps and number of frames
+full_name = FILENAME + ".mp4"
+cap = cv2.VideoCapture(os.path.join(VIDEO_PATH, full_name))
 fps = cap.get(cv2.CAP_PROP_FPS)
-length = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
+number_of_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
 
-start_sec = 4
-end_sec = 5
+# Calculate start and end frames given start and end seconds and video fps
 start_frame = int(START_SEC * fps)
 end_frame = END_SEC * fps
-if end_frame > length:
-    end_frame = length
+if end_frame > number_of_frames:
+    end_frame = number_of_frames
 
 
-
+# Add image preprocessing
 transform = T.Compose(
     [
         T.RandomResize([800], max_size=1333),
@@ -52,21 +55,24 @@ transform = T.Compose(
 )
 
 i = 0
-
-# image_source, image = load_image(IMAGE_PATH)
 while(cap.isOpened()):
     ret, frame = cap.read()
+    
+    # if current frame is before start frame read next one 
     if i < start_frame:
         i += 1
         continue
-    image_source = frame.copy()
+    
+    # Copy frame as numpy ndarray
+    image_source = frame.copy() 
 
-    width, height, _ = image_source.shape
-
+    # Convert frame to PIL Image
     image = Image.fromarray(frame).convert("RGB")
 
+    # Preprocess image
     image, _ = transform(image, None)
 
+    # Detect objects and calculate inference time
     start_time = time()
     boxes, logits, phrases = predict(
         model=model,
@@ -78,7 +84,7 @@ while(cap.isOpened()):
     )
     print(f"Inference time: {time() - start_time}")
 
-
+    # Save bboxes
     bboxes = []
     if boxes.size()[0] != 0:
         for box in boxes:
@@ -86,17 +92,20 @@ while(cap.isOpened()):
             bbox_y_center = box[1].item()
             bbox_height = box[2] .item()
             bbox_width = box[3].item()
-
             bboxes.append([bbox_x_center, bbox_y_center, bbox_height, bbox_width])
 
+
+    # Draw bounding boxes
     annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
 
+    # Get maximum index in images output dir
     existing_indexes = [int(index[:-4]) for index in os.listdir(images_output_dir)]
     if len(existing_indexes) == 0:
         max_index = -1
     else:
         max_index = max(existing_indexes)
-    annotation = {"filename": full_name, "image_name": f"frame_{i}", "bboxes": bboxes}
+        
+    # Save outputs
     cv2.imwrite(os.path.join(annotated_output_dir, f"{max_index+1}.jpg"), annotated_frame)
     cv2.imwrite(os.path.join(images_output_dir, f"{max_index+1}.jpg"), image_source)
     with open(os.path.join(labels_output_dir, f"{max_index+1}.txt"), "w+") as f:
@@ -107,5 +116,6 @@ while(cap.isOpened()):
 
     print("Processed frame index:", i)
 
+    # If frame is equal to end frame brake the loop 
     if i > end_frame:
         break
