7e6baeb9c69b9d29f05e4498d0f97feb76943225
PyroX2 <kubawilk63@gmail.com>
2024-08-05 16:48:25 +0200

    Cleaning

--- tracker.py
+++ tracker.py
@@ -3,21 +3,6 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
-def drawRectangle(frame, bbox):
-    p1 = (int(bbox[0]), int(bbox[1]))
-    p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-    cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-
-# Draws rectangle on frame and displays it
-def displayRectangle(frame, bbox):
-    plt.figure(figsize=(20, 10))
-    frameCopy = frame.copy()
-    drawRectangle(frameCopy, bbox)
-    frameCopy = cv2.cvtColor(frameCopy, cv2.COLOR_RGB2BGR)
-    plt.imshow(frameCopy)
-    plt.show()
-    plt.axis("off")
-
 
 class Tracker:
     def __init__(self, text_prompt: str) -> None:
